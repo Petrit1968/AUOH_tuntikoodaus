539cb0e6ad09fa4814f471ecc810624a9e083b88
Petrit1968 <petri.taipale@seamk.fi>
2022-01-28 19:01:50 +0200

    commit

--- 06_webstocket/app.js
+++ 06_webstocket/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
@@ -10,33 +10,31 @@ let number_of_connections = 0;
 
 const broadcast_message = (message)=>{
     for(let id in connections){
-        const stocket = connections[id];
+        const socket = connections[id];
         socket.emit("server-to-client", message);
     }
 };
 
-
 io.on("connection", (socket)=>{
     connections[socket.id] = socket;
     number_of_connections++;
-
-    // socket.emit("server-to-client", "Hello from server");
-
+    //socket.emit("server-to-client", "Hello from server");
     console.log("client connected. number_of_connections: ", number_of_connections);
     socket.on("disconnect", (socket)=>{
         delete connections[socket.id];
         number_of_connections--;
-        console.log("client connected. number_of_connections: ", number_of_connections);
-  
-    })
+        console.log("client disconnected. number_of_connections: ", number_of_connections);
+        //
+    });
 
     socket.on("client-to-server", (message)=>{
-        console.log(message);
-    })
+        broadcast_message(message);
+        //console.log(message);
+    });
 
-
+    
 });
 
 app.use(express.static("public"));
-server.listen(PORT);
 
+server.listen(PORT);
