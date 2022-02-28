ad172aab4e87890db9a529451f0f4fafad9521d1
Petrit1968 <petri.taipale@seamk.fi>
2022-02-28 14:27:09 +0200

    commit

---
+++ 10_fanuc_viewer/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("piublic"));
+app.use(express.static("node_modules/three/build"));
+
+app.listen(PORT);
+console.log("Listening port: ", PORT);
