8cbf5af8470d56885e83a0b65931bf0979b1a33c
Petrit1968 <petri.taipale@seamk.fi>
2022-04-08 15:46:40 +0300

    commit

--- 11_rest_api/app.js
+++ 11_rest_api/app.js
@@ -11,14 +11,18 @@ app.use(body_parser.json());
 // CREATE
 
 app.post("/api/alarm", alarm_controller.api_post_alarm);
+app.get("/api/alarms", alarm_controller.api_get_alarms);
+app.get("/api/alarm/:id", alarm_controller.api_get_alarm);
+app.put("/api/alarm/:id", alarm_controller.api_put_alarm);
 
 const db_uri = "mongodb+srv://db_user:QGoVzA46XiVaX8Uw@cluster0.9iggy.mongodb.net/alarm_db?retryWrites=true&w=majority";
 
-mongoose.connect(db_uri, {}).then(() =>{
+
+mongoose.connect(db_uri, {}).then(() => {
     console.log("db connected");
-    console.log("listening port: ", PORT)
+    console.log("listening port: ", PORT);
     app.listen(PORT);
-});
+  });
 
 
 
